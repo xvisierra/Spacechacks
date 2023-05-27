be58b5c4a4ad3aecc5bf6443a004afee1a80da55
Sameer Kumar Singh <xvisierra@gmail.com>
2023-05-27 18:07:44 +0530

    misc

--- scripts/game.js
+++ scripts/game.js
@@ -11,10 +11,45 @@ const info = document.querySelector('.eraInfo')
 const chars = [
     './images/astronaut.png',
     './images/2.png',
-    './images/3.gif',
-    './images/dino.gif',
+    './images/3.png',
+    './images/4.png',
     './images/5.png',
     './images/6.png',
+    './images/6.png',
+    './images/7.png',
+    './images/astronaut.png',
+    './images/2.png',
+    './images/3.png',
+    './images/4.png',
+    './images/5.png',
+    './images/6.png',
+    './images/6.png',
+    './images/7.png',
+    './images/astronaut.png',
+    './images/2.png',
+    './images/3.png',
+    './images/4.png',
+    './images/5.png',
+    './images/6.png',
+    './images/6.png',
+    './images/7.png',
+    './images/astronaut.png',
+    './images/2.png',
+    './images/3.png',
+    './images/4.png',
+    './images/5.png',
+    './images/6.png',
+    './images/6.png',
+    './images/7.png',
+    './images/astronaut.png',
+    './images/2.png',
+    './images/3.png',
+    './images/4.png',
+    './images/5.png',
+    './images/6.png',
+    './images/6.png',
+    './images/7.png',
+      
     
 ]
 
@@ -33,6 +68,18 @@ const eras = [
 ]
 
     const eraInfo = [
+        `The Solar System is the gravitationally bound system of the Sun and the objects that orbit it, either directly or indirectly. Of the objects that orbit the Sun directly, the largest are the eight planets, with the remainder being smaller objects, such as dwarf planets and small Solar System bodies.`,
+        `The Milky Way is a barred spiral galaxy with a diameter between 100,000 and 150,000 light-years. It is estimated to contain 100–400 billion stars and more than 100 billion planets.`,
+        `A black hole is a region of spacetime where gravity is so strong that nothing, not even light, can escape. Black holes are formed from the remnants of a large star that has collapsed in on itself. They can have masses ranging from a few times that of the Sun to billions of times its mass.`,
+        `Organizations have proposed plans for a human mission to Mars, the first step towards any colonization effort, but no person has set foot on the planet, and there have been no return missions. However, landers and rovers have successfully explored the planetary surface and delivered information about conditions on the ground.`,
+        `The Hubble Space Telescope is a space telescope that was launched into low Earth orbit in 1990 and remains in operation. It is one of the largest and most versatile space telescopes, renowned both as a vital research tool and as a public relations boon for astronomy.`,
+        `Perseverance, nicknamed Percy, is a car-sized Mars rover designed to explore the Jezero crater on Mars as part of NASA’s Mars 2020 mission. It was manufactured by the Jet Propulsion Laboratory and launched on July 30, 2020`,
+        `The Solar System is the gravitationally bound system of the Sun and the objects that orbit it, either directly or indirectly. Of the objects that orbit the Sun directly, the largest are the eight planets, with the remainder being smaller objects, such as dwarf planets and small Solar System bodies.`,
+        `The Milky Way is a barred spiral galaxy with a diameter between 100,000 and 150,000 light-years. It is estimated to contain 100–400 billion stars and more than 100 billion planets.`,
+        `A black hole is a region of spacetime where gravity is so strong that nothing, not even light, can escape. Black holes are formed from the remnants of a large star that has collapsed in on itself. They can have masses ranging from a few times that of the Sun to billions of times its mass.`,
+        `Organizations have proposed plans for a human mission to Mars, the first step towards any colonization effort, but no person has set foot on the planet, and there have been no return missions. However, landers and rovers have successfully explored the planetary surface and delivered information about conditions on the ground.`,
+        `The Hubble Space Telescope is a space telescope that was launched into low Earth orbit in 1990 and remains in operation. It is one of the largest and most versatile space telescopes, renowned both as a vital research tool and as a public relations boon for astronomy.`,
+        `Perseverance, nicknamed Percy, is a car-sized Mars rover designed to explore the Jezero crater on Mars as part of NASA’s Mars 2020 mission. It was manufactured by the Jet Propulsion Laboratory and launched on July 30, 2020`,
         `The Solar System is the gravitationally bound system of the Sun and the objects that orbit it, either directly or indirectly. Of the objects that orbit the Sun directly, the largest are the eight planets, with the remainder being smaller objects, such as dwarf planets and small Solar System bodies.`,
         `The Milky Way is a barred spiral galaxy with a diameter between 100,000 and 150,000 light-years. It is estimated to contain 100–400 billion stars and more than 100 billion planets.`,
         `A black hole is a region of spacetime where gravity is so strong that nothing, not even light, can escape. Black holes are formed from the remnants of a large star that has collapsed in on itself. They can have masses ranging from a few times that of the Sun to billions of times its mass.`,
@@ -110,7 +157,7 @@ var checkDead = setInterval(() => {
         header.innerText = eras[i]
         info.innerText = eraInfo[i]
         character.style.backgroundImage = `url(${chars[i]})`
-        setTimeout(incre, 5000)
+        setTimeout(incre, 3000)
         } else {
             document.querySelector(".gameon").style.display = 'block'
             document.querySelector(".gameover").style.display = 'block'
